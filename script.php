5df1964ff5aa3c591a3283dfc2a6056e33f45104
Ben Foose <foosechek@gmail.com>
2013-10-16 23:47:33 -0400

    snapshot of server. 5 game setup

--- script.php
+++ script.php
@@ -1,11 +1,11 @@
 <?php
+date_default_timezone_set('America/New_York');
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 // get the "message" variable from the post request
 // this is the data coming from the Android app
-var_dump($_POST);
 if(empty($_POST['id'])){
-    echo "avoid the noid";
+    echo "noid";
 } else if ($_POST['id'] == "START") {
     $opponent=$_POST['opponent'];
     $game=$_POST['game'];
@@ -15,7 +15,7 @@ if(empty($_POST['id'])){
         fprintf($file,"%s\t%s", $opponent, $today);
         fclose($file);
         
-        for($num=1;$num<=3;$num++){
+        for($num=1;$num<=5;$num++){
             $filename = "game" . strval($num) . ".txt"; 
             $file = fopen($filename,"w");
             fprintf($file,"%d\t%d","0", "0");
@@ -43,8 +43,18 @@ if(empty($_POST['id'])){
     $g3h = $fields[0];
     $g3a = $fields[1];
     fclose($file);
-    $outfile = fopen("archive.html", "a+");
-    fwrite($outfile,"<q>Match start:" . $time ."</q><br>\n");
+    $file = fopen("game4.txt","r");
+    $fields = fgetcsv($file, 100, "\t");
+    $g4h = $fields[0];
+    $g4a = $fields[1];
+    fclose($file);
+    $file = fopen("game5.txt","r");
+    $fields = fgetcsv($file, 100, "\t");
+    $g5h = $fields[0];
+    $g5a = $fields[1];
+    fclose($file);
+    $outfile = fopen("archive_temp.html", "w+");
+    fwrite($outfile,"<p1>Match start:" . $time ."</p1>\n");
     fwrite($outfile,"<table style=\"text-align: left; width: 600px; height: 60px;\" border=\"1\" cellpadding=\"2\" cellspacing=\"2\">\n");
     fwrite($outfile,"<tbody>\n");
     fwrite($outfile,"<tr>\n");
@@ -52,18 +62,24 @@ if(empty($_POST['id'])){
     fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g1h . "<br>  </td>\"\n");
     fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g2h . "<br>  </td>\"\n");
     fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g3h . "<br>  </td>\n");
+    fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g4h . "<br>  </td>\n");
+    fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g5h . "<br>  </td>\n");
     fwrite($outfile,"</tr>\n");
     fwrite($outfile,"<tr>\n");
     fwrite($outfile,"<td style=\"vertical-align: top; width: 402px;\">" . $opponent . "<br>  </td>\n");
     fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g1a . "<br> </td>\n");
     fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g2a . "<br> </td>\n");
     fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g3a . "<br> </td>\n");
+    fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g4h . "<br>  </td>\n");
+    fwrite($outfile,"<td style=\"vertical-align: top; width: 66px;\">" . $g5h . "<br>  </td>\n");
     fwrite($outfile,"</tr>\n");
     fwrite($outfile,"</tbody>\n");
     fwrite($outfile,"</table>\n");
     fwrite($outfile,"<br><br>\n");
 
-    fclose($outfile);
+    fclose($outfile); 
+    shell_exec('cat archive_temp.html > temp.html && cat archive_scores.html >> temp.html && mv temp.html archive_scores.html');
+    shell_exec('cat archive_header.html > archive.html && cat archive_scores.html >> archive.html');
 } else if ($_POST['id'] == "SCORE") {
    $home=$_POST['home']; 
    $away=$_POST['away']; 
@@ -72,11 +88,11 @@ if(empty($_POST['id'])){
    $file = fopen($filename,"w");
    fprintf($file,"%d\t%d",$home, $away);
    fclose($file);    
-} else if ($_PORT['id'] == "MESSAGE") {
-   $message=$_POST['message']; 
-   $file = fopen("message.txt", "w");
-   fprintf($file,"%s", $message);
-   fclose($file)
+} else if ($_POST['id'] == "MESSAGE") {
+   $message=$_POST['message'];
+   $outfile = fopen("message.txt", "w+");
+   fprintf($outfile,">%s",$message);
+   fclose($outfile);
 }
 ?>
 
